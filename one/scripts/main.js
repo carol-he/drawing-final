60e066192520423b4f92073acd80d858328e8c78
Carol He <ch2727@nyu.edu>
2018-05-01 13:04:43 -0400

    cloud logic

--- one/scripts/main.js
+++ one/scripts/main.js
@@ -8,7 +8,7 @@ var speeds = [];
 
 for(var i = 0; i < clouds.length; i++){
   leftpos.push(Math.floor((Math.random() * 5) - 1) + i*15);
-  speeds.push(((Math.random() * 2) + 2) * .01)
+  speeds.push(((Math.random() * 2) + 2) * .01 + .01);
 }
 
 
@@ -17,7 +17,11 @@ function moveClouds(){
      leftpos[i] += speeds[i];
     clouds[i].style.left = leftpos[i] + '%'; // move div by 5 pixels each time
     if(leftpos[i] > 100)
-      leftpos[i] = -30;
+      if(i == 2 || i == 5 || i == 6){
+        leftpos[i] = -15;
+      } else {
+        leftpos[i] = -30;
+      }
   }
   requestAnimationFrame(moveClouds);
 }
--- two/scripts/main.js
+++ two/scripts/main.js
@@ -6,16 +6,20 @@ var speeds = [];
 
 for(var i = 0; i < clouds.length; i++){
   leftpos.push(Math.floor((Math.random() * 5) - 1) + i*15);
-  speeds.push(((Math.random() * 2) + 2) * .005)
+  speeds.push(((Math.random() * 2) + 2) * .01 + .01);
 }
 
 
 function moveClouds(){
   for (var i = 0; i < clouds.length; i++) {
-    leftpos[i] += speeds[i];
+     leftpos[i] += speeds[i];
     clouds[i].style.left = leftpos[i] + '%'; // move div by 5 pixels each time
     if(leftpos[i] > 100)
-      leftpos[i] = -30;
+      if(i == 2 || i == 4){
+        leftpos[i] = -15;
+      } else {
+        leftpos[i] = -30;
+      }
   }
   requestAnimationFrame(moveClouds);
 }
